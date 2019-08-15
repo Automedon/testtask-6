4026dfdf0154a09b3f02b3b443b38ce9beb1bac6
Automedon <42299107+Automedon@users.noreply.github.com>
2019-08-15 17:42:04 +0700

    done3

---
+++ docs/precache-manifest.a0b56b3cc57627040d3c95ee4d7d897b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06fd072f57e073ea00a3111a95a81eab",
+    "url": "/testtask-6/index.html"
+  },
+  {
+    "revision": "65b14a14ca96f8e86834",
+    "url": "/testtask-6/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "65b14a14ca96f8e86834",
+    "url": "/testtask-6/static/js/2.c34defb1.chunk.js"
+  },
+  {
+    "revision": "4380b8031062130c9563",
+    "url": "/testtask-6/static/js/main.ddb8f5f1.chunk.js"
+  },
+  {
+    "revision": "0d2735f5432cd2532d40",
+    "url": "/testtask-6/static/js/runtime~main.bbd91454.js"
+  },
+  {
+    "revision": "0b5a011d423203ef8d79da4be7bdbc30",
+    "url": "/testtask-6/static/media/960x576_1.0b5a011d.webp"
+  },
+  {
+    "revision": "163982911d653410d0e162d3f6ac81a9",
+    "url": "/testtask-6/static/media/nc_ss19_620x960px_1.16398291.webp"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f65cf538678c9db6dce6494f1178d097.js"
+  "/testtask-6/precache-manifest.a0b56b3cc57627040d3c95ee4d7d897b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/testtask-6/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
